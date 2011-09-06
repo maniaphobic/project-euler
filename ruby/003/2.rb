02c036b4feb2f1e5fb4005768c9002b763efd888
metameme <metameme@maniaphobic.org>
2011-09-06 14:49:47 -0700

    Checkpoint commit.  After much work on the Sieve of Erastothanes approach, I realized generating a quadrillion integers will exhaust virtual memory.  I need to find a method relatively efficient in both space and time.

--- ruby/003/2.rb
+++ ruby/003/2.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/env ruby1.9
 
-def sieve(n)
+#
+# This function implements the Sieve of Erastothanes
+#
+
+def prime_sieve(n)
   primes = []
   range = (2..n).to_a
   range_size = range.size
+  puts "Range size=#{range_size}"
   cursor = 0
   p = range[cursor]
   while p < n
-#DEBUG#    puts "p=#{p}" #DEBUG#
+    puts p
     primes << p
     (2*p).step(n, p) {|i| range[i-2] = -1}
-#DEBUG#print range, "\n"#DEBUG#
-#DEBUG#puts "cursor=#{cursor}, range[cursor]=#{range[cursor]}"
     cursor += 1
     while cursor < range_size and range[cursor] < 0
       cursor += 1
@@ -23,14 +26,16 @@ def sieve(n)
 end
 
 #
-#
+# Use the sieve to find all primes less than the square root of the
+# target number.  Divide each into the target and emit those that
+# divide evenly.
 #
 
 n = ARGV[0].to_i
-#DEBUG#sqrt = Math.sqrt(n).ceil
+sqrt = Math.sqrt(n).ceil
 factors = []
-sieve(n).each do |p|
-#DEBUG#  break if p > sqrt
+prime_sieve(n/2+1).each do |p|
+  puts p
   factors << p if n%p == 0
 end
-print factors.join(", "), "\n"
+print "#{factors.count}: ", factors.join(", "), "\n"
